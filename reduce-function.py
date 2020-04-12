32903dd48126e4da02ec3cd5943ceaaa4b3333d6
Mayur Goyal <Mayurg6832.mg@gmail.com>
2020-04-13 03:15:22 +0530

    Create reduce-function.py

---
+++ reduce-function.py
@@ -0,0 +1,14 @@
+from fractions import Fraction
+from functools import reduce
+
+def product(fracs):
+    mul= lambda x,y:x*y
+    t=reduce(mul,fracs)
+    return t.numerator, t.denominator
+
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
