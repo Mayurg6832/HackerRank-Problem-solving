cd63f5c8170dcbcd80213f856c7f99d9dd2327a8
Mayur Goyal <Mayurg6832.mg@gmail.com>
2020-05-09 19:15:53 +0530

    Create Intro to Tutorial Challenges.py

---
+++ Intro to Tutorial Challenges.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the introTutorial function below.
+def introTutorial(V, arr):
+    for i in range(len(arr)):
+        if arr[i]==V:
+            return i
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    V = int(input())
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = introTutorial(V, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
