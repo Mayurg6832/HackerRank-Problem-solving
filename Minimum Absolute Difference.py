13cfd8a2aeefecb4366f196bf9fffa920de6adab
Mayur Goyal <Mayurg6832.mg@gmail.com>
2020-05-28 04:21:06 +0530

    Create Minimum Absolute Difference.py

---
+++ Minimum Absolute Difference.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr.sort()
+    mini=float('inf')
+    for i in range(len(arr)-1):
+        if abs(arr[i]-arr[i+1])<mini:
+            mini=abs(arr[i]-arr[i+1])
+    return mini
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
